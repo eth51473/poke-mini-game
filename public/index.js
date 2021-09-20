1f9672430f4076acac2e3d4612473d9b2205d4a7
Ethan Bird <eth51473@gmail.com>
2021-09-20 15:51:30 -0600

    fixed display bug where size of everything changed when gifs displayed

--- public/index.js
+++ public/index.js
@@ -6,11 +6,13 @@ const searchBtn = document.getElementById('searchBtn')
 const lvl = document.getElementById('lvl')
 const screen = document.getElementById('screen')
 const textBox = document.getElementById('text-box')
+const catchBtn = document.getElementById('catchBtn')
+const fightBtn = document.getElementById('fightBtn')
 let pokeName = ''
 let playerXp = 0
 let sprite=''
 let playerLvl = 0
-const levels = [0,100,500,1000,2000,6000,10000]
+const levels = [0,50,100,175,250,500,1000,2000,6000,10000]
       document.getElementById("searchBtn").onclick = function () {
         let rand = Math.floor(Math.random() * 890);
         axios.get(`https://pokeapi.co/api/v2/pokemon/${rand}`)
@@ -18,9 +20,9 @@ const levels = [0,100,500,1000,2000,6000,10000]
               screen.style.backgroundImage = `url('https://www.digitaleidoscope.com//wp-content/uploads/2015/06/ps_02_04.gif')`
               // pokePic.style.width = '20vw'
               pokePic.style.backgroundImage = 'none'
-              textBox.style.display = 'none'
+              textBox.style.visibility = 'hidden'
               setTimeout(() => {
-                textBox.style.display = 'inline'
+                textBox.style.visibility = 'visible'
                 screen.style.backgroundImage = 'none'
                 pokePic.style.width = '16vw'
                 pokeName = response.data.name
@@ -28,6 +30,7 @@ const levels = [0,100,500,1000,2000,6000,10000]
                 pokePic.style.backgroundImage = 
                 `url('${sprite}')`
                 appearMsg.textContent = `${pokeName} has appeared`
+                
                 searchBtn.textContent = "run away"
                 choiceBtns.style.display = 'inline' }, 1500);
               
@@ -47,15 +50,16 @@ const levels = [0,100,500,1000,2000,6000,10000]
       document.getElementById('catchBtn').addEventListener('click',catchPokemon)
       function catchPokemon() {
         if(appearMsg.textContent.endsWith("appeared")){
+
           screen.style.backgroundImage = `url('https://media2.giphy.com/media/AE6BTS3QZ5OdUI9zxT/200w.webp?cid=ecf05e47psxmi9btf424ctvvcnreoh1zby1z0fw1qe1mspol&rid=200w.webp&ct=g')`
-          textBox.style.display = 'none'
+          textBox.style.visibility = 'hidden'
           pokePic.style.backgroundImage = 'none'
          setTimeout(() => { 
-          textBox.style.display = 'inline'
+          textBox.style.visibility = 'visible'
           screen.style.backgroundImage ='none'
           pokePic.style.backgroundImage = `url('https://www.freepnglogos.com/uploads/pokeball-png/pokeball-alexa-style-blog-pokemon-inspired-charmander-daily-8.png')` }, 750);
          searchBtn.textContent = "Search"
-         choiceBtns.style.display = 'none'
+         
         let newPoke = document.createElement('h3')
         newPoke.textContent = pokeName
         pokeStorage.appendChild(newPoke)
@@ -83,11 +87,11 @@ const levels = [0,100,500,1000,2000,6000,10000]
         if(appearMsg.textContent.endsWith("appeared")){
           
           screen.style.backgroundImage = `url('https://thumbs.gfycat.com/AridArtisticBassethound.webp')`
-          textBox.style.display = 'none'
+          textBox.style.visibility = 'hidden'
           pokePic.style.backgroundImage = 'none'
               pokePic.style.width = '25vw'
               setTimeout(() => {
-                textBox.style.display = 'inline'
+                textBox.style.visibility = 'visible'
                 screen.style.backgroundImage ='none'
               pokePic.style.backgroundImage = 
               `url('${sprite}')`
@@ -95,17 +99,17 @@ const levels = [0,100,500,1000,2000,6000,10000]
               setTimeout(() => {
                 pokePic.style.backgroundImage = 
               `url('https://img.favpng.com/5/3/24/ash-ketchum-pokxe9mon-go-pokxe9mon-ruby-and-sapphire-pikachu-misty-png-favpng-yk4QpKB0R2FnEi4aSayacxA6C_t.jpg')`
-                appearMsg.textContent = `you have earned ${playerXp} xp`
+                
               },1000)
               let battleXp = Math.floor((Math.random() * 100)+50);
               playerXp += battleXp
+              appearMsg.textContent = `you have earned ${battleXp} xp`
               for (let i = 0; i<levels.length; i++){
                 if(playerXp < levels[i]){
                   lvl.textContent = `Player lvl: ${i-1}`
                   return
                 }
               }
-              choiceBtns.style.display = 'none'
               
             },6200);
           
